e1e7c303daac0d79fd571286e0c542c5c1997d05
dhwang21 <hwang.derrick@gmail.com>
2018-11-28 21:56:29 -0800

    added in third test and passed

--- BinarySearch.java
+++ BinarySearch.java
@@ -6,19 +6,23 @@ public class BinarySearch{
     public static void main(String[] args){
         int[] testArray1 = {4,8,15,16,23,42};
         int[] testArray2 = {11,22,33,44,55,66,77};
+        int[] testArray3 = {};
 
         int testKey1 = 15;
         int testKey2 = 90;
+        int testKey3 = 90;
 
         int expectedOutPutTest1 = 2;
         int expectedOutPutTest2 = -1;
+        int expectedOutPutTest3 = -1;
 
         int binaryResult1 = binarySearch(testKey1, testArray1);
         int binaryResult2 = binarySearch(testKey2, testArray2);
+        int binaryResult3 = binarySearch(testKey3, testArray3);
 
         System.out.println("Expected: " + expectedOutPutTest1 + " Result: " + binaryResult1);
         System.out.println("Expected: " + expectedOutPutTest2 + " Result: " + binaryResult2);
-
+        System.out.println("Expected: " + expectedOutPutTest3 + " Result: " + binaryResult3);
 
     }
 
