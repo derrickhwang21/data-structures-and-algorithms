c61858940589a89acb49678d92c707bacce69e3e
dhwang21 <hwang.derrick@gmail.com>
2018-12-02 15:51:44 -0800

    finished assertion test

--- src/main/java/ArrayShift.java
+++ src/main/java/ArrayShift.java
@@ -8,17 +8,6 @@ public class ArrayShift{
         int[] testArray2 = {1,2,3,5,24};
         int[] testArray3 = {12,4,5,6};
 
-        int testValue = 3;
-
-        System.out.println("Add middle index:");
-        System.out.println("Original Array: " + Arrays.toString(testArray1) + " New Array: " + Arrays.toString(insertShiftArray(testArray1, testValue)));
-        System.out.println("Original Array: " + Arrays.toString(testArray2) + " New Array: " + Arrays.toString(insertShiftArray(testArray2, testValue)));
-        System.out.println("Original Array: " + Arrays.toString(testArray3) + " New Array: " + Arrays.toString(insertShiftArray(testArray3, testValue)));
-        System.out.println("");
-        System.out.println("Remove middle index:");
-        System.out.println("Original Array: " + Arrays.toString(testArray1) + " New Array: " + Arrays.toString(removeShiftArray(testArray1)));
-        System.out.println("Original Array: " + Arrays.toString(testArray2) + " New Array: " + Arrays.toString(removeShiftArray(testArray2)));
-        System.out.println("Original Array: " + Arrays.toString(testArray3) + " New Array: " + Arrays.toString(removeShiftArray(testArray3)));
     }
 
     public static int[] insertShiftArray (int[] originalArray, int inputValue){
