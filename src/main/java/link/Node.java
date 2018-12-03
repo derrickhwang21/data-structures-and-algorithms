3e5715e325fda5be41b6bf363cb47c0ea6c565ed
dhwang21 <hwang.derrick@gmail.com>
2018-12-03 13:36:39 -0800

    finished print and ind include method

---
+++ src/main/java/link/Node.java
@@ -0,0 +1,35 @@
+package link;
+
+public class Node {
+      Object data;
+      Node next;
+
+
+     public Node(Object data, Node next){
+         this.data = data;
+         this.next = next;
+     }
+
+//     public Node(Object dataValue, int nextValue){
+//         next = nextValue;
+//         data = dataValue;
+//     }
+//
+//     public Object getData(){
+//         return data;
+//     }
+//
+//     public void setData(Object datavalue){
+//         data = dataValue;
+//     }
+//
+//     public Node getNext(){
+//         return next;
+//     }
+//
+//     public void setNext(Node nextValue){
+//         next = nextValue;
+//     }
+
+
+}
