a0eb3c55202d3ac2b27dd72e3eb03b05f32f2c6d
dhwang21 <hwang.derrick@gmail.com>
2018-12-17 13:28:23 -0800

    added in binarySearchTree class

--- src/main/java/Tree/Tree.java
+++ src/main/java/Tree/Tree.java
@@ -3,38 +3,8 @@ package Tree;
 public class Tree {
         Node root;
 
-    /**
-     * Insertion of a new node
-     *
-     * If the new node's value is lower than the current node's, we go to the left child
-     * If the new node's value is greater than the current node's we go the right child
-     * when the current node is null, we've reached a leaf node and we can insert the new node in that position
 
-     * @return
-     */
-    public Node addRecursive(Node current, int value){
-        if (current == null){
-            return new Node(value);
-        }
 
-        if (value < current.value){
-            current.left = addRecursive(current.left, value);
-        }
-        else if (value > current.value){
-            current.right = addRecursive(current.right, value)
-        }
-        else{
-            return current;
-        }
-        return current;
-    }
-
-    /**
-     * starts recursion from the root node
-     */
-    public void add(int value){
-        root = addRecursive(root, value);
-    }
 
 
     /**
