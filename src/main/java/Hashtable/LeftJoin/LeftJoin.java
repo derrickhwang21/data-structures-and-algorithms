11a65d43da861a0e6c574b8c074d52fe4f00860c
dhwang21 <hwang.derrick@gmail.com>
2019-01-23 02:02:18 -0800

    finished challenge

--- src/main/java/Hashtable/LeftJoin/LeftJoin.java
+++ src/main/java/Hashtable/LeftJoin/LeftJoin.java
@@ -1,4 +1,27 @@
 package Hashtable.LeftJoin;
 
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LeftJoin {
+
+    public static String[][] leftJoin(HashMap<String, String> synonymMap, HashMap<String, String> antonymMap){
+        List<String> keys = new ArrayList<>(synonymMap.keySet());
+        
+        String[][]result = new String[keys.size()][3];
+        for(int i  = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            result[keys.size() - 1 - i][0] = key;
+            result[keys.size() - 1 - i][1] = synonymMap.get(key);
+            if (antonymMap.containsKey(key)) {
+                result[keys.size() - 1 - i][2] = antonymMap.get(key);
+            } else {
+                result[keys.size() - 1 - i][2] = null;
+            }
+        }
+        return result;
+    }
 }
---
+++ src/test/java/Hashtable/LeftJoin/LeftJoinTest.java
@@ -0,0 +1,74 @@
+package Hashtable.LeftJoin;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static Hashtable.LeftJoin.LeftJoin.leftJoin;
+import static org.junit.Assert.*;
+
+public class LeftJoinTest {
+
+    @Test
+    public void leftJoinTestWithMatchingKeys() {
+
+        HashMap<String, String> synonyms = new HashMap<String, String>();
+        HashMap<String, String> antonyms = new HashMap<String, String>();
+
+        synonyms.put("fond", "enamored");
+        synonyms.put("wrath", "anger");
+        synonyms.put("diligent", "employed");
+        antonyms.put("fond", "averse");
+        antonyms.put("wrath", "delight");
+        antonyms.put("diligent", "idle");
+
+        String[][] results = leftJoin(synonyms, antonyms);
+        String[][] expected = new String[][]{{"fond", "enamored", "averse"}, {"wrath", "anger", "delight"}, {"diligent", "employed", "idle"}};
+
+        assertArrayEquals("must be the same", expected, results);
+
+
+    }
+
+    @Test
+    public void leftJoinTestWithSomeMismatchingKeys() {
+
+        HashMap<String, String> synonyms = new HashMap<String, String>();
+        HashMap<String, String> antonyms = new HashMap<String, String>();
+
+        synonyms.put("fond", "enamored");
+        synonyms.put("wrath", "anger");
+        synonyms.put("diligent", "employed");
+        antonyms.put("fond", "averse");
+        antonyms.put("blueberries", "delight");
+        antonyms.put("diligent", "idle");
+
+        String[][] results = leftJoin(synonyms, antonyms);
+        String[][] expected = new String[][]{{"fond", "enamored", "averse"}, {"wrath", "anger", null}, {"diligent", "employed", "idle"}};
+
+        assertArrayEquals("must be the same", expected, results);
+
+
+    }
+
+    @Test
+    public void leftJoinTestWithNonMatchingKeys() {
+
+        HashMap<String, String> synonyms = new HashMap<String, String>();
+        HashMap<String, String> antonyms = new HashMap<String, String>();
+
+        synonyms.put("fond", "enamored");
+        synonyms.put("wrath", "anger");
+        synonyms.put("diligent", "employed");
+        antonyms.put("fondue", "averse");
+        antonyms.put("wrathful", "delight");
+        antonyms.put("diligently", "idle");
+
+        String[][] results = leftJoin(synonyms, antonyms);
+        String[][] expected = new String[][]{{"fond", "enamored", null}, {"wrath", "anger", null}, {"diligent", "employed", null}};
+
+        assertArrayEquals("must be the same", expected, results);
+
+
+    }
+}
